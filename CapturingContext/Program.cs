565119e1839b55aa18922e1d963f7d6f15ceffe8
Santiago Blanco Leis <sablanc@microsoft.com>
2019-10-17 14:07:14 -0700

    Added EmptyBodyAsyncOverhead project.

---
+++ CapturingContext/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapturingContext
+{
+    class Program
+    {
+        const int ITERS = 50000;
+
+        static void Main(string[] args)
+        {
+            Form f = new Form() { Width = 400, Height = 300 };
+            Button b = new Button() { Text = "Run", Dock = DockStyle.Fill, Font = new Font("Consolas", 18) };
+            f.Controls.Add(b);
+
+            b.Click += async delegate
+            {
+                b.Text = "... Running ... ";
+                await Task.WhenAll(WithSyncCtx(), WithoutSyncCtx()); // warm-up
+
+                Stopwatch sw = new Stopwatch();
+
+                sw.Restart();
+                await WithSyncCtx();
+                System.TimeSpan withTime = sw.Elapsed;
+
+                sw.Restart();
+                await WithoutSyncCtx();
+                System.TimeSpan withoutTime = sw.Elapsed;
+
+                b.Text = string.Format("With    : {0}\nWithout : {1}\n\nDiff    : {2:F2}x",
+                    withTime, withoutTime, withTime.TotalSeconds / withoutTime.TotalSeconds);
+            };
+
+            f.ShowDialog();
+        }
+        
+
+        private static async Task WithSyncCtx()
+        {
+            for (int i = 0; i < ITERS; i++)
+            {
+                Task t = Task.Run(() => { });
+                await t;
+            }
+        }
+
+        private static async Task WithoutSyncCtx()
+        {
+            for (int i = 0; i < ITERS; i++)
+            {
+                Task t = Task.Run(() => { });
+                await t.ConfigureAwait(continueOnCapturedContext: false);
+            }
+        }
+
+    }
+}
---
+++ EmptyBodyAsyncOverhead/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmptyBodyAsyncOverhead
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch sw = new Stopwatch();
+            const int ITERS = 10000000;
+
+            EmptyBody();
+            EmptyBodyAsync();
+
+            while (true)
+            {
+                sw.Restart();
+                for (int i = 0; i < ITERS; i++)
+                {
+                    EmptyBody();
+                }
+                TimeSpan emptyBodyTime = sw.Elapsed;
+
+                sw.Restart();
+                for (int i = 0; i < ITERS; i++)
+                {
+                    EmptyBodyAsync();
+                }
+                TimeSpan emptyBodyAsyncTime = sw.Elapsed;
+
+                Console.WriteLine("Sync  : {0}", emptyBodyTime);
+                Console.WriteLine("Async : {0}", emptyBodyAsyncTime);
+                Console.WriteLine("-- {0:F1}x --", emptyBodyAsyncTime.TotalSeconds / emptyBodyTime.TotalSeconds);
+            }
+        }
+
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void EmptyBody()
+        { }
+
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static async Task EmptyBodyAsync()
+        { }
+
+      
+    }
+}
